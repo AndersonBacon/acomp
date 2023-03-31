7f8f9249e78134171550a07589407f5693a8d68b
Anderson Christo Cunha <105126487+AndersonBacon@users.noreply.github.com>
2023-03-31 18:31:07 +0000

    1,2,3,4

--- 1/ex4.py
+++ 1/ex4.py
@@ -1,12 +1,37 @@
-from itertools import permutations  
+from itertools import permutations
 
-from functools import reduce
-def sortString(str): 
-    return reduce(lambda a, b : a + b, sorted(str)) 
-   
-str = str(input("Digite a string: "))
-print(sortString(str))
+def busca_r(v, s, i=0):
+  if i == len(v):
+    return -1
+  if v[i] == s:
+    return i + 1
+  return busca_r(v, s, i + 1)
 
-p = permutations(sortString(str))  
-for j in list(p):  
-    print(j) 
+def busca_i(v, s):
+  for i, p in enumerate(v):
+    if p == s:
+      return i + 1
+  return -1
+
+def procura(b, s):
+  v = [str().join(p) for p in list(permutations(list(s)))]
+  v = sorted([*set(v)])
+  return b(v, s)
+
+s = 'bacaa'
+r = procura(busca_r, s)
+print(r)
+r = procura(busca_i, s)
+print(r)
+
+s = 'abc'
+r = procura(busca_r, s)
+print(r)
+r = procura(busca_i, s)
+print(r)
+
+s = 'cba'
+r = procura(busca_r, s)
+print(r)
+r = procura(busca_i, s)
+print(r)
